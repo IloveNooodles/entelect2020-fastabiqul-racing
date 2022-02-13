7b0fd85f9fc8fb99cadc21c7fb996bf0286921c7
gagaspbahar <gagaspbahar@gmail.com>
2022-02-14 00:25:29 +0700

    v0 ggbot

--- java/src/main/java/za/co/entelect/challenge/Bot.java
+++ java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -13,7 +13,7 @@ import java.security.SecureRandom;
 
 public class Bot {
 
-    private static final int maxSpeed = 9;
+    private static final int maxSpeed = 15;
     private List<Command> directionList = new ArrayList<>();
 
     private final Random random;
@@ -37,46 +37,81 @@ public class Bot {
     public Command run(GameState gameState) {
         Car myCar = gameState.player;
         Car opponent = gameState.opponent;
+        int myCurrentSpeed = myCar.speed;
 
-        //Basic fix logic
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
-        List<Object> nextBlocks = blocks.subList(0,1);
+        // Basic fix logic
+        List<Object> allVisibleBlocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        List<Object> blocks = getBlocksInFrontBySpeed(allVisibleBlocks, myCurrentSpeed);
+        List<Object> blocksRight = getBlocksInFront(myCar.position.lane + 1, myCar.position.block, gameState);
+        List<Object> blocksLeft = getBlocksInFront(myCar.position.lane - 1, myCar.position.block, gameState);
+        List<Object> usualBlocks = blocks.subList(0, 9);
+        List<Object> nextBlocks = blocks.subList(0, 1);
 
-        //Fix first if too damaged to move
-        if(myCar.damage == 5) {
+        // Fix first if too damaged to move
+        if (myCar.damage == 5) {
             return FIX;
         }
-        //Accelerate first if going to slow
-        if(myCar.speed <= 3) {
+        // Accelerate first if going to slow
+        if (myCar.speed <= 3) {
             return ACCELERATE;
         }
 
-        //Basic fix logic
-        if(myCar.damage >= 5) {
+        // Basic fix logic
+        if (myCar.damage >= 5) {
             return FIX;
         }
 
-        //Basic avoidance logic
-        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+        // Basic avoidance logic
+        if (blocks.contains(true) || blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
             if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
                 return LIZARD;
             }
             if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-                int i = random.nextInt(directionList.size());
-                return directionList.get(i);
+                if (myCar.position.lane == 1) {
+                    return TURN_RIGHT;
+                } else if (myCar.position.lane == 4) {
+                    return TURN_LEFT;
+                } else {
+                    if(myCar.position.lane == 2){
+                        if(getBlocksInFrontBySpeed(blocksRight, myCurrentSpeed).contains(Terrain.MUD)){
+                            return TURN_LEFT;
+                        }
+                        else{
+                            return TURN_RIGHT;
+                        }
+                    }
+                    else if(myCar.position.lane == 3){
+                        if(getBlocksInFrontBySpeed(blocksLeft, myCurrentSpeed).contains(Terrain.MUD)){
+                            return TURN_RIGHT;
+                        }
+                        else{
+                            return TURN_LEFT;
+                        }
+                    }
+                }
             }
         }
 
-        //Basic improvement logic
+        // Basic improvement logic
         if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
             return BOOST;
         }
 
-        //Basic aggression logic
+        // Basic aggression logic
         if (myCar.speed == maxSpeed) {
             if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
                 return OIL;
             }
+            if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+                return EMP;
+            }
+            if (hasPowerUp(PowerUps.TWEET, myCar.powerups)) {
+                return new TweetCommand(opponent.position.lane, opponent.position.block + 2 * opponent.speed);
+            }
+        }
+
+        if (opponent.position.lane == myCar.position.lane || opponent.position.lane == myCar.position.lane + 1
+                || opponent.position.lane == myCar.position.lane - 1) {
             if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
                 return EMP;
             }
@@ -86,7 +121,7 @@ public class Bot {
     }
 
     private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
-        for (PowerUps powerUp: available) {
+        for (PowerUps powerUp : available) {
             if (powerUp.equals(powerUpToCheck)) {
                 return true;
             }
@@ -98,6 +133,7 @@ public class Bot {
      * Returns map of blocks and the objects in the for the current lanes, returns
      * the amount of blocks that can be traversed at max speed.
      **/
+
     private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
         List<Lane[]> map = gameState.lanes;
         List<Object> blocks = new ArrayList<>();
@@ -113,6 +149,10 @@ public class Bot {
 
         }
         return blocks;
+    }
+
+    private List<Object> getBlocksInFrontBySpeed(List<Object> blocks, int speed){
+        return blocks.subList(0, speed);
     }
 
 }
